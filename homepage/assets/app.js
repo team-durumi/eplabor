8af1b9ceaa08799eb9f7bd1635df590cced5d24d
홍영택 <mozo@mozo-pro.local>
2020-07-14 22:27:16 +0900

    무료 상담 신청을 ajax 형식으로 전환합니다.

--- homepage/assets/app.js
+++ homepage/assets/app.js
@@ -19,5 +19,28 @@ window.addEventListener('DOMContentLoaded', function() {
         'body': "이상하군 왜 되는거지?"
     });
 
+    var httpRequest;
+    document.getElementById("consulting-request-submit").addEventListener('click', function(e) {
+        e.preventDefault();
+        httpRequest = new XMLHttpRequest();
+        if(!httpRequest) {
+            alert('상담을 처리할 수 없습니다. 센터로 전화주세요.');
+            return false;
+        }
+        httpRequest.onreadystatechange = alertContents;
+        httpRequest.open('POST', 'http://admin.eplabor.org/eplabor_admin/items/enquiry');
+        httpRequest.send(new FormData( document.forms['consulting-request'] ));
+    });
+  
+    function alertContents() {
+      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+          alert('정상적으로 접수됐습니다. 담당자가 연락드리겠습니다.');
+        } else {
+          alert('필수 항목을 입력해주세요');
+        }
+      }
+    }
+
 });
 
