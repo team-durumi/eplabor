ff0e2a2aee1b02c730766e4ee477a2e1d6575256
yeongtaek, Hong <mozodev@users.noreply.github.com>
2020-08-01 23:43:31 +0900

    프로비저닝 스크립트에 실행 권한을 추가합니다.

--- provision/directus.sh
+++ provision/directus.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 function download_latest_directus() {
+    rm -rf directus
     curl -s https://api.github.com/repos/directus/directus/releases/latest \
     | grep tarball_url \
     | cut -d '"' -f 4 \
     | xargs -n 1 curl -sL \
-    | tar zxf - \
-    && mv directus-* directus
-    && rm directus/.gitignore
+    | tar zxf - 
+    mv directus-* directus
+    rm /vagrant/directus/.gitignore
+    git checkout -- ./
 }
 
-download_latest_directus()
+download_latest_directus
