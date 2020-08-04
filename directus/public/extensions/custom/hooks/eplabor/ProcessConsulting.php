7f8a1a891e489b1c86afd5e78be476474f45f159
yeongtaek, Hong <mozodev@users.noreply.github.com>
2020-08-04 12:23:26 +0900

    상담 요청 관련 훅 코드를 정리합니다.

--- directus/public/extensions/custom/hooks/eplabor/ProcessConsulting.php
+++ directus/public/extensions/custom/hooks/eplabor/ProcessConsulting.php
@@ -1,11 +1,10 @@
 <?php
 /**
- * ProcessConsultingAsMarkdown
+ * ProcessConsulting
  * 
  * 상담 생성, 수정 시 넘어온 상담 데이터를 휴고 콘텐츠 디렉토리 내 마크다운 파일로 저장 그리고, 커밋, 푸시한다.
  * directus custom hook for creating and updating eplabor_consultings
  * 
- * @return void
  */
 
 namespace Directus\Custom\Hooks\eplabor;
@@ -22,15 +21,24 @@ class ProcessConsulting implements HookInterface {
     private $type;
     private $base_path;
     private $view;
+    private $tableGateway;
+    private $config;
 
     public function __construct($data, $type){
-        $this->container = \Directus\Application\Application::getInstance();
-        $this->logger = $this->container->fromContainer('logger');
+        $this->container = \Directus\Application\Application::getInstance()->getContainer();
+        $this->logger = $this->container->get('logger');
         $this->slack_base_uri = getenv('SLCAK_WEBHOOK_ENDPOINT') ?: '';
         $this->slack_payload = [ 'text' => '' ];
         $this->type = $type;
-        $this->base_path = $this->container->fromContainer('path_base');
+        $this->base_path = $this->container->get('path_base');
         $this->view = new Twig([ $this->base_path . '/public/extensions/custom/hooks/eplabor/' ]);
+        $this->tableGateway = \Directus\Database\TableGatewayFactory::create('eplabor_consultings', [
+            'connection' => $this->container->get('database'),
+            'acl' => false
+        ]);
+        $app = \Directus\create_app_with_project_name('/vagrant/directus', 'eplabor');
+        $this->config = $app->getConfig();
+
         $this->handle($data);
     }
 
@@ -41,41 +49,35 @@ class ProcessConsulting implements HookInterface {
      * @return void
      */
     public function handle($data = null) {
-        
+        // 요청 형식에 따라 실행
+        // $this->logger->debug('handle!!');
         if(!empty($this->type) && in_array($this->type, ['create', 'update'])) {
             $this->{$this->type}($data);
         }
-
-        // git commit and push
-        // chdir('/vagrant/');
-        // // https://github.com/simonthum/git-sync
-        // $output = shell_exec('git-sync');
-        // $this->logger->debug($output);
-
-        // 슬랙 웹훅 엔드포인트가 있는 경우 노티
-        if(!empty($this->slack_base_uri)) {
-            $client = new \GuzzleHttp\Client([
-                'base_uri' => $this->slack_base_uri
-            ]);
-            $this->slack_payload['text'] .= $markdown_text;
-            $response = $client->post('', [ 'body' => json_encode($this->slack_payload) ]);
-        }
         
+        if($this->config->get('env') != 'development') {
+            $this->gitPush();
+        }
     }
 
     private function create($data = null) {
-        $markdown_string = $this->buildMarkdown($data);
+        $markdown_string = $this->slack_payload['text'] = $this->buildMarkdown($data);
         // 디스크에 마크다운 파일로 저장
         file_put_contents('/vagrant/homepage/content/consulting/online/' . $data['id'] . '.md', $markdown_string);
-        $this->logger->debug('/vagrant/homepage/content/consulting/online/' . $data['id'] . '.md');
+        // $this->logger->debug('/vagrant/homepage/content/consulting/online/' . $data['id'] . '.md');
     }
 
     private function update($data = null) {
-        $this->logger->debug('update()--------');
-        $this->logger->debug(print_r($this->data, true));
-        if(!empty($data) && $data['status'] == 'deleted') {
+        // $this->logger->debug('update()--------');
+        // $this->logger->debug(print_r($data, true));
+        if(!empty($data['status']) && $data['status'] == 'deleted') {
             $output = shell_exec('rm -f /vagrant/homepage/content/consulting/online/' . $data['id'] . '.md');
-            $this->logger->debug($output . ' deleted?');
+            // $this->logger->debug($output . ' deleted?');
+        } else {
+            // $this->create($data);
+            // $this->logger->debug(print_r($this->tableGateway, true));
+            $item = $this->tableGateway->getOneData($data['id']);
+            $this->create($item);
         }
     }
 
@@ -90,4 +92,26 @@ class ProcessConsulting implements HookInterface {
 
         return $this->view->fetch('markdown.twig', $data);
     }
+
+    private function slackNotify($data) {
+        // 슬랙 웹훅 엔드포인트가 있는 경우 노티
+        if(!empty($this->slack_base_uri)) {
+            $client = new \GuzzleHttp\Client([
+                'base_uri' => $this->slack_base_uri
+            ]);
+            $this->slack_payload['text'] .= $markdown_text;
+            $response = $client->post('', [ 'body' => json_encode($this->slack_payload) ]);
+        }
+    }
+
+    private function gitPush() {
+        // git commit and push
+        chdir('/vagrant/');
+        // // https://github.com/simonthum/git-sync
+        // $output = shell_exec('git-sync');
+        $this->logger->debug('git pushed!');
+        // $this->logger->debug($output);
+    }
+
 }
+?>
--- directus/public/extensions/custom/hooks/eplabor/hooks.php
+++ directus/public/extensions/custom/hooks/eplabor/hooks.php
@@ -1,9 +1,13 @@
 <?php
 
+require_once 'ProcessConsulting.php';
+
 return [
     'actions' => [
-        'item.create.eplabor_consultings' => function($data){ new \Directus\Custom\Hooks\eplabor\ProcessConsulting($data, 'create'); },
-        'item.update.eplabor_consultings' => function($data){ new \Directus\Custom\Hooks\eplabor\ProcessConsulting($data, 'update'); },
-        'item.delete.eplabor_consultings' => function($data){ new \Directus\Custom\Hooks\eplabor\ProcessConsulting($data, 'delete'); },
-    ]
+        'item.create.eplabor_consultings' => function ($data) {new Directus\Custom\Hooks\eplabor\ProcessConsulting($data, 'create');},
+        'item.update.eplabor_consultings' => function ($data) {new Directus\Custom\Hooks\eplabor\ProcessConsulting($data, 'update');},
+        'item.delete.eplabor_consultings' => function ($data) {new Directus\Custom\Hooks\eplabor\ProcessConsulting($data, 'delete');},
+    ],
 ];
+
+?>
