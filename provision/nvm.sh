d06c8c4f172e5cca5a52fbff896b4d55af5c3a68
yeongtaek, Hong <mozodev@users.noreply.github.com>
2020-08-08 13:12:40 +0900

    패스에 노드 바이너리 경로를 추가합니다.

--- provision/nvm.sh
+++ provision/nvm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+export PATH=./node_modules/.bin:$PATH
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
